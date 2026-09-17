90ef000a01a21704b95825ca39f1c87515096a22
Werner Krenn <93549501+WernerKr@users.noreply.github.com>
2026-09-17 17:28:51 +0200

    Add files via upload

--- skins/new-belchertown2.1.1/bin/new_belchertown.py
+++ skins/new-belchertown2.1.1/bin/new_belchertown.py
@@ -55,7 +55,7 @@ if weewx.__version__ < "5":
 
 log = logging.getLogger(__name__)
 
-VERSION = "2.1.1_20260911"
+VERSION = "2.1.1_20260916"
 
 # Print version in syslog for easier troubleshooting
 log.info("version %s", VERSION)
@@ -8743,6 +8743,9 @@ class getData(SearchList):
             ]
         )
         highcharts_lang = _highcharts_lang_options(skin_dict)
+        records_week_time_format = self.generator.formatter.time_format_dict.get(
+            "week", weewx.units.DEFAULT_TIME_FORMAT
+        )
 
         # Build the search list with the new values
         search_list_extension = {
@@ -8757,6 +8760,7 @@ class getData(SearchList):
             "moment_locale_js": moment_locale_js,
             "dayjs_locale_js": dayjs_locale_js,
             "locale_encoding": locale_encoding,
+            "records_week_time_format": records_week_time_format,
             "highcharts_decimal": highcharts_decimal,
             "highcharts_thousands": highcharts_thousands,
             "radar_html": radar_html,
